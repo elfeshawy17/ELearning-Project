116a28a47bd75c88faef576b0b152556ab20c448
Mohamed Elfeshawy <mh3386240@gmail.com>
2025-03-04 05:23:38 +0200

    added user model

---
+++ data/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['student', 'professor', 'admin'],
+        default: 'student'
+    },
+    passwordChangedAt: Date,
+    courses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+
+userSchema.pre('save', function(next){
+    if (this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, 8);
+    }
+    next();
+});
+
+
+export const User = mongoose.model('User', userSchema);
