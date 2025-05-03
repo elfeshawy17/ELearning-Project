d5ef16736b261ce144e34e5515bf701d69630f32
mohamed tamer <mohamed.tamer3355@gmail.com>
2025-05-03 22:56:46 +0300

    add payment

---
+++ data/models/payment.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+ 
+const paymentSchema = new Schema({
+    studentId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    courses: [{
+        type: Schema.Types.ObjectId,
+        ref: "Course",
+        required: true
+    }],
+    totalFee: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    hasPaid: {
+        type: Boolean,
+        default: false
+    },
+    hourlyRate: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+export const Payment = model("Payment", paymentSchema);
